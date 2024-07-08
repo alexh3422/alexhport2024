51a7d4af34ddf62bb46315aada18d9c66136ef12
Alexander Hall <alexanderhall@Alexanders-iMac.local>
2024-07-08 10:11:06 -0700

    updated text

--- src/layouts/PageIntro.tsx
+++ src/layouts/PageIntro.tsx
@@ -6,7 +6,7 @@ export function PageIntro() {
     <>
       <div className="flex flex-col justify-center align-center text-center mb-10 mt-[100px]">
         <div className="flex justify-center">
-          <motion.div whileHover={{scale: 1.2}}>
+          <motion.div whileHover={{ scale: 1.2 }}>
             <img src={Emoji} className="h-[300px] w-[300px]" alt="alex emoji" />
           </motion.div>
         </div>
@@ -15,7 +15,10 @@ export function PageIntro() {
             My name is Alex Hall, I am a full stack web developer who is a
             graduate from the University of Washington's Full Stack Web
             Development Program. <br /> <br />
-            Please feel free to browse around and view my work below.
+            My goal is to craft efficient code that produces visually appealing
+            and intuitive products.
+            <br /> <br /> Please feel free to browse around and view my work
+            below.
           </h1>
         </div>
       </div>
